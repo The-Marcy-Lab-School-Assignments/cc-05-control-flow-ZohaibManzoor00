b239cbccb02aa14a9847f973f5897e80d9931e99
Zohaib <zohaibmanzoor1700@gmail.com>
2021-10-06 14:39:11 +0000

    Completed Codes

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 //1
-function absoluteValue(){
-  
+const absoluteValue = (num) => {
+    if (typeof num === 'number') { 
+        if (num < 0) {
+            return num;
+        } else {
+            return num*-1;
+            } 
+               } else {
+                   return null;
+    } 
 }
 
 
+console.log(absoluteValue(-20))
 
 
+const multiplesOfFourAndSix = () => {
+    let array = [];
+    for (let i = 1; i <= 100; i++){
+        if (i % 4 === 0 && i % 6 === 0){
+            array.push(i)
+        }
+    } 
+    return array;
+}
+
+console.log(multiplesOfFourAndSix())
